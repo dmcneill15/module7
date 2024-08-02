9f9a3fbf2c8227f3672bf57847db693379948d93
dmcneill15 <dvveijeren@gmail.com>
2024-08-03 09:39:23 +1200

    exercise 1

--- exercise1_useEffect/src/BitcoinRates.jsx
+++ exercise1_useEffect/src/BitcoinRates.jsx
@@ -7,7 +7,7 @@ export function BitcoinRates() {
     const [bitCoinPrice, setBitCoinPrice] = useState(0);
 
     useEffect(()=>{
-        let ignore = false; //flag used to ignore fetch results
+        let ignore = false; //flag used to ignore fetch results that are no longer valid
         
         fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=' + currency)
         .then(response => response.json())
@@ -17,23 +17,14 @@ export function BitcoinRates() {
             } //if ignore is false, get the results
             });
 
-        //cleanup function to run when unmounted or when currency changes
+        //cleanup function to run when unmounted or when currency changes (before useEffect is run again)
         return() =>{
             ignore = true;  //ignore the fetch results because they are now invalid
             console.log('Cleaned up');
         }
     }, [currency]); //include currency here to cause the effect to run eveytime the currentcy changes
 
-    // fetch URL: https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}
-    /*/ returns: {"bitcoin":
-                    {
-                      "usd":68626,
-                      "aud":12345,
-                      "nzd":12345
-                    }
-                  } 
-        To access this will do json.bitcoin[usd] will return 68626
-    */
+
     const options = currencies.map(curr => <option value={curr} key={curr}>{curr}</option>);
     return (
         <div className="BitcoinRates componentBox">
@@ -47,3 +38,14 @@ export function BitcoinRates() {
         </div>
     )
 }
+
+// fetch URL: https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}
+    /*/ returns: {"bitcoin":
+                    {
+                      "usd":68626,
+                      "aud":12345,
+                      "nzd":12345
+                    }
+                  } 
+        To access this will do json.bitcoin[usd] will return 68626
+    */
